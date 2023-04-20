82827679eb75510eed454e5dcae187b234611692
Gillian Zech <ginger01@email.unc.edu>
2023-04-20 15:13:17 -0400

    Got results up next to color picker

--- src/Home.js
+++ src/Home.js
@@ -39,51 +39,20 @@ export function Home() {
   const handleType = (e) => {
     setType(e.target.value);
   }
+
   return (
     <div style = {{width: "100%"}}>
       <Header/>
-      
-      {/* <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-        <TabList>
-          <Tab>Hex Code</Tab>
-          <Tab>Color Palette</Tab>
-          <Tab>RGB</Tab>
-        </TabList>
-      </Tabs>  */}
-      <div>
-      <div className='flex-container'>
-          <div className='flex-items'>
-            {/* <Button name="Hex"/>
-            <Button name="Color Palette"/>
-            <Button name="Rgb"/> */}
-            {/* <div> */}
-              {/* <TabPanel forceRender='true'> */}
-                {/* <input
-                  type="text"
-                  id="message"
-                  name="message"
-                  onChange={handleChange}
-                  hideHSV 
-                /> */}
-              {/* </TabPanel> */}
-              {/* </div> */}
-              <div style = {{margin: "20px"}}>
-                {/* <TabPanel default= "true"> */}
-                  <ColorPicker width = {456} height = {228}
-                        color = {color}
-                        onChange = {setColor}
-                        hideRGB = {true}
-                        hideHSV = {true} dark 
-                        >      
-                  </ColorPicker>
-                {/* </TabPanel> */}
-              </div>
-              {/* <div> */}
-                {/* <Rgb/> */}
-              {/* </div> */}
-        {/* </div> */}
-        {/* <div className='col mb-3'> */}
-          {/* <div className='d-flex align-items-center justify-content-evenly  click'> */}
+        <div className='flex-container'>
+          <div style = {{margin: "20px"}}>
+            <ColorPicker width = {456} height = {228}
+                  color = {color}
+                  onChange = {setColor}
+                  hideRGB = {true}
+                  hideHSV = {true} dark 
+                  >      
+            </ColorPicker>
+          </div>
           <div style = {{ width: "456px", height: "260px", margin: "20px"}}>
               <select value={options.value} onChange={handleType}
               className='text-dark'>
@@ -91,21 +60,15 @@ export function Home() {
                   <option value={option.value}>{option.label}</option>
                 ))}
               </select>
-            <div className='row'>
-              <h1 style = {{textAlign: "center"}}><strong>Result</strong></h1>
-              <Result col={color.hex} type={type}></Result>
-            </div>
-            {/* </div> */}
-          {/* </div> */}
+              <div className='row'>
+                <h1 style = {{textAlign: "center"}}><strong>Result</strong></h1>
+                <Result col={color.hex} type={type}></Result>
+              </div>
           </div>
         </div>
-      </div>
-      </div>
-
       <div className='border-0 accordion-flush' style = {{marginBottom: "7%"}}>      
         <Description/>
       </div>
-  </div>
-
+    </div>
   )
 } 
